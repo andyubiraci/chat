432719e980dfd7e4ad50c63940511fae90d912bb
Andy Ubiraci <andy@mktgteam.com>
2018-01-15 21:59:40 -0600

    xc

--- server/main.js
+++ server/main.js
@@ -4,6 +4,8 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
+app.use(express.static('public'))
+
 app.get('/', function(req, res){
     res.status(200).send("MKTG Chat...!");
 })
