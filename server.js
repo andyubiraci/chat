b238232221d5dc3d719fa21eb6db51fd51b32a7d
Andy Ubiraci <andy@mktgteam.com>
2018-01-15 23:56:33 -0600

    server

---
+++ server.js
@@ -0,0 +1,35 @@
+var express= require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+var messages = [{
+    id:1,
+    text: "Mi mensaje",
+    author: "Andy Ubiraci" 
+}];
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.status(200).send("MKTG Chat...!");
+});
+
+io.on('connection', function(socket) {
+    console.log('Alguien en Sockets');
+    socket.emit('messages',messages);
+    
+    
+    socket.on('new-message', function(data){
+        
+        messages.push(data);
+        io.sockets.emit('messages', messages);
+        
+    });
+    
+});
+
+server.listen(80, function(){
+    console.log("corriendo..!");
+});
