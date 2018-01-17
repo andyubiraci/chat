73279a6d0949fd43f24e625a05acce11a14408aa
Andy Ubiraci <andy@mktgteam.com>
2018-01-17 13:13:00 -0600

    rooms

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ let http = require('http').Server(app);
 let io = require('socket.io')(http);
  
 io.on('connection', (socket) => {
+    
+    
+    
+      socket.on('room', function(room) {
+        socket.join(room);
+    });
   
   socket.on('disconnect', function(){
     io.emit('users-changed', {user: socket.nickname, event: 'left'});   
@@ -16,6 +22,12 @@ io.on('connection', (socket) => {
   socket.on('add-message', (message) => {
     io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
   });
+    
+    
+    
+    
+    
+    
 });
  
 var port = process.env.PORT || 3000;
