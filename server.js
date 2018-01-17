bb9c8e93846c8c2773496ffab8f2c904663e704f
Andy Ubiraci <andy@mktgteam.com>
2018-01-17 15:14:13 -0600

    io.join

--- server.js
+++ server.js
@@ -5,17 +5,13 @@ let io = require('socket.io')(http);
 io.on('connection', (socket) => {
     
     
-    
-      socket.on('join:room', function(data) {
-      
-          alert(data.room_name);
-          
-          var room_name = data.room_name;
-        socket.join(room_name);
-          
- console.log('someone joined room ' + room_name + ' ' + socket.id);          
-          
-    });
+
+socket.on('join:room', function(data) {
+alert(data.room_name);
+var room_name = data.room_name;
+io.join(room_name);
+console.log('someone joined room ' + room_name + ' ' + socket.id);          
+});
   
   socket.on('disconnect', function(){
     io.emit('users-changed', {user: socket.nickname, event: 'left'});   
